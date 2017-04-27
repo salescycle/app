9efe3410b9f822e098e71b8e3054e45162067d07
Matt Blake <mattblake82@gmail.com>
2017-04-27 17:54:39 -0500

    Styling the new chevrons

--- src/components/clientList/clientlist.jsx
+++ src/components/clientList/clientlist.jsx
@@ -1,9 +1,16 @@
 import React from 'react'
-import { elementType } from 'react-prop-types';
+import {elementType} from 'react-prop-types';
 import Chevron from './components/chevron.jsx'
 import Styles from './clientList.css'
 
+
 export default function ClientList(props) {
+		const one = 'Website'
+		const two = 'Closing'
+		const three = 'TBD'
+		const four = 'Factfinder'
+		const five = 'Approach'
+		const six = 'Telephone'
 
 	return (
 		<div className={`${Styles.clientRowDiv} row`}>
@@ -11,23 +18,25 @@ export default function ClientList(props) {
 				<span className={Styles.clientName}>Bill Brasky</span>
 			</div>
 			<div className="columns small-10">
-			<div className="columns small-2">
-			<Chevron className="" chevronBackground={Styles.chevronBackground} chevronText='Telephone'/>
-			</div>
-			<div className="columns small-2">
-			<Chevron chevronBackground={Styles.chevronBackground} chevronText='Approach'/>
-			</div>
-			<div className="columns small-2">
-			<Chevron chevronBackground={Styles.chevronBackground} chevronText='Factfinder'/>
-			</div>
-			<div className="columns small-2">
-			<Chevron chevronBackground={Styles.chevronBackground} chevronText='TBD'/>
-			</div>
-			<div className="columns small-2">
-			<Chevron chevronBackground={Styles.chevronBackground} chevronText='Closing'/>
-			</div>
-			<div className="columns small-2">
-			<Chevron chevronBackground={Styles.chevronBackground} chevronText='Website'/>
+			<div className='steps-container'>
+				<div className='steps'>
+					<span>{one}</span>
+				</div>
+				<div className='steps'>
+					<span>{two}</span>
+				</div>
+				<div className='steps'>
+					<span>{three}</span>
+				</div>
+				<div className='steps'>
+					<span>{four}</span>
+				</div>
+				<div className='steps'>
+					<span>{five}</span>
+				</div>
+				<div className='steps'>
+					<span>{six}</span>
+				</div>
 			</div>
 			</div>
 		</div>
--- src/components/home/home.jsx
+++ src/components/home/home.jsx
@@ -4,18 +4,18 @@ import PropTypes from 'prop-types'
 import ClientListContainer from '../../container/client/clientList.js'
 import Styles from './home.css'
 
-export default function Home(props){
+export default function Home(props) {
 	let {handleClick} = props
 	return (
 		<div>
-		<div className="row collapse">
-		<div className="columns small-8">
-				<Header titleText="| Might Morphin Power Client Syncer"></Header>
+			<div className="row collapse">
+				<div className="columns small-8">
+					<Header titleText="| Might Morphin Power Client Syncer"></Header>
 				</div>
-				</div>
-				<div className={Styles.mainMenu}>
-					<ClientListContainer />
-				</div>
+			</div>
+			<div className={Styles.mainMenu}>
+				<ClientListContainer/>
+			</div>
 		</div>
 	)
 }
