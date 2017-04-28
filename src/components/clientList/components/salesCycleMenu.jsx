0ba36eaf81c04b1efbc3aed92b05462503384c3e
Matt Blake <mattblake82@gmail.com>
2017-04-27 20:38:18 -0500

    Styling menu innards
    Styling innards

--- src/components/clientList/components/salesCycleMenu.jsx
+++ src/components/clientList/components/salesCycleMenu.jsx
@@ -5,10 +5,27 @@ export default function SalesCycleMenu(props) {
 	var menuDivFF = Styles.menuDivFactfinding
 	var menuDivPrep = Styles.menuDivPreparation
 	return (
-		<div className={menuDivPrep}>
-			<div className={Styles.crmTitle}>CRM</div>
-
-			<div className={Styles.matchesTitle}>Potential Matches</div>
+		<div className={menuDivFF}>
+			<div className={Styles.CRMDiv}>
+				<div className={Styles.crmTitle}>CRM</div>
+				<div className={Styles.crmDataName}>
+					<span>Bill</span>
+					<span>Braskulina</span>
+				</div>
+				<ul className={Styles.crmDataInfo}>
+					<li>12/02/1973</li>
+					<li>123 Terry Dr. WI, 53291</li>
+					<li>M</li>
+				</ul>
+			</div>
+			<div className={Styles.matchesDiv}>
+				<div className={Styles.matchesTitle}>Potential Matches</div>
+				<input className={Styles.checkbox} type="checkbox"/>
+				<span>Bob Braskolinaowsiki</span>
+				<span>123 Terri Dr. WI, 53291</span>
+				<span>M</span>
+				<span>.50</span>
+			</div>
 		</div>
 	)
 }
